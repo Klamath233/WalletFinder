d59a3a477f01b4c0f7a9d2af545de6f2eac430c4
Xi Han <xihan94@ucla.edu>
2016-10-17 10:22:45 -0700

    It looks like an app now...

--- WalletFinder/AppDelegate.swift
+++ WalletFinder/AppDelegate.swift
@@ -13,10 +13,16 @@ import UserNotifications
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
-
+    var lastKnownLatitude: Double? = nil
+    var lastKnownLongitude: Double? = nil
+    var defaults: UserDefaults! = nil
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 		
+        defaults = UserDefaults.standard
+        lastKnownLatitude = defaults.object(forKey: "lastKnownLatitude") as? Double
+        lastKnownLongitude = defaults.object(forKey: "lastKnownLongitude") as? Double
+        
 		// Initialize Gimbal framework.
 		Gimbal.setAPIKey("1ce025fb-6823-43a3-9fb1-a5fdc476518e", options: nil)
 		Gimbal.start()
@@ -35,10 +41,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		
 		return true
 	}
+    
+    private func storeLastKnownLocation() {
+        if let lat = lastKnownLatitude, let lng = lastKnownLongitude {
+            defaults.set(lat, forKey: "lastKnownLatitude")
+            defaults.set(lng, forKey: "lastKnownLongitude")
+        }
+    }
 
 	func applicationWillResignActive(_ application: UIApplication) {
 		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
 		// Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        storeLastKnownLocation()
 	}
 
 	func applicationDidEnterBackground(_ application: UIApplication) {
@@ -48,6 +62,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func applicationWillEnterForeground(_ application: UIApplication) {
 		// Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        
 	}
 
 	func applicationDidBecomeActive(_ application: UIApplication) {
@@ -56,6 +71,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func applicationWillTerminate(_ application: UIApplication) {
 		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        storeLastKnownLocation()
 	}
 
 
--- WalletFinder/MainScreenViewController.swift
+++ WalletFinder/MainScreenViewController.swift
@@ -8,16 +8,27 @@
 
 import UIKit
 import UserNotifications
+import MapKit
 
 class MainScreenViewController: UIViewController, GMBLPlaceManagerDelegate, GMBLBeaconManagerDelegate {
 
+    var _appDelegate: AppDelegate! = nil
 	var _manager: GMBLBeaconManager! = nil
 	var _placeManager: GMBLPlaceManager! = nil
+    var _dateFormatter: DateFormatter! = nil
+    var _locationManager: CLLocationManager! = nil
+    var _pin: MKPointAnnotation! = nil
 	
+    @IBOutlet weak var signalLabel: UILabel!
+    @IBOutlet weak var signalDateLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
+    
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
-		
+        
+        _appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
 		_manager = GMBLBeaconManager()
 		_manager.delegate = self
 		_manager.startListening()
@@ -25,9 +36,34 @@ class MainScreenViewController: UIViewController, GMBLPlaceManagerDelegate, GMBL
 		_placeManager = GMBLPlaceManager()
 		_placeManager.delegate = self
 		
+        _dateFormatter = DateFormatter()
+        _dateFormatter.timeZone = NSTimeZone.local
+        _dateFormatter.dateStyle = .medium
+        _dateFormatter.timeStyle = .long
+        
+        _locationManager = CLLocationManager()
+        _locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        
+        _pin = MKPointAnnotation()
+        _pin.title = "Wallet"
+        
+        updateCoord()
+        
 		print("view did load")
 	}
-
+    
+    private func updateCoord() {
+        if let lat = _appDelegate.lastKnownLatitude, let lng = _appDelegate.lastKnownLongitude {
+            _pin.coordinate = CLLocationCoordinate2D(latitude: lat, longitude: lng)
+            mapView.addAnnotation(_pin)
+            
+            mapView.setRegion(MKCoordinateRegionMakeWithDistance(_pin.coordinate, 500, 500), animated: true)
+            mapView.setCenter(_pin.coordinate, animated: true)
+        } else {
+            mapView.removeAnnotation(_pin)
+        }
+    }
+    
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
@@ -35,46 +71,52 @@ class MainScreenViewController: UIViewController, GMBLPlaceManagerDelegate, GMBL
 	
 	func beaconManager(_ manager: GMBLBeaconManager!, didReceive sighting: GMBLBeaconSighting!) {
 		if (sighting.beacon.identifier == "9HHA-R7F2Q") { // my wallet beacon
-			let startedNotification = UNMutableNotificationContent()
-			startedNotification.title = "Wallet Finder"
-			startedNotification.body = "Your wallet is nearby! Signal strength: \(sighting.rssi)"
-			startedNotification.categoryIdentifier = "message"
-			UNUserNotificationCenter.current().add(UNNotificationRequest(identifier: "did_receive_sighting", content: startedNotification, trigger: nil), withCompletionHandler: nil)
+            signalDateLabel.text = _dateFormatter.string(from: sighting.date)
+            
+            if sighting.rssi < -80 {
+                signalLabel.textColor = UIColor.red
+                signalLabel.text = String("Lost Wallet 😭")
+                sendWalletLostNotification()
+            } else {
+                signalLabel.textColor = UIColor.green
+                signalLabel.text = String(sighting.rssi)
+            }
+            
+            _appDelegate.lastKnownLatitude = _locationManager.location?.coordinate.latitude
+            _appDelegate.lastKnownLongitude = _locationManager.location?.coordinate.longitude
+            
+            updateCoord()
 		}
 	}
+    
+    private func sendWalletLostNotification() {
+        let notification = UNMutableNotificationContent()
+        notification.title = "Wallet Finder"
+        notification.body = "You are getting far away from your wallet."
+        notification.categoryIdentifier = "message"
+        UNUserNotificationCenter.current().add(UNNotificationRequest(identifier: "wallet_lost", content: notification, trigger: nil), withCompletionHandler: nil)
+    }
 	
-	func placeManager(_ manager: GMBLPlaceManager!, didEnd visit: GMBLVisit!) {
-		if (visit.place.identifier == "4C79341D0DC34F7F88B82436DF975DC6")  { // my wallet place
-			let startedNotification = UNMutableNotificationContent()
-			startedNotification.title = "Wallet Finder"
-			startedNotification.body = "You left your wallet behind! Last known location: \(visit.place.attributes)"
-			startedNotification.categoryIdentifier = "message"
-			UNUserNotificationCenter.current().add(UNNotificationRequest(identifier: "did_end_visit", content: startedNotification, trigger: nil), withCompletionHandler: nil)
-
-		}
-	}
-	
-	func placeManager(_ manager: GMBLPlaceManager!, didBegin visit: GMBLVisit!) {
-		if (visit.place.identifier == "4C79341D0DC34F7F88B82436DF975DC6")  { // my wallet place
-			let startedNotification = UNMutableNotificationContent()
-			startedNotification.title = "Wallet Finder"
-			startedNotification.body = "Your wallet is nearby! Last known location: \(visit.place.attributes)"
-			startedNotification.categoryIdentifier = "message"
-			UNUserNotificationCenter.current().add(UNNotificationRequest(identifier: "did_begin_visit", content: startedNotification, trigger: nil), withCompletionHandler: nil)
-			
-		}
-	}
-	
-	func placeManager(_ manager: GMBLPlaceManager!, didReceive sighting: GMBLBeaconSighting!, forVisits visits: [AnyObject]!) {
-		if (sighting.beacon.identifier == "9HHA-R7F2Q") { // my wallet beacon
-			let startedNotification = UNMutableNotificationContent()
-			startedNotification.title = "Wallet Finder"
-			startedNotification.body = "Your wallet is nearby! Signal strength: \(sighting.rssi)"
-			startedNotification.categoryIdentifier = "message"
-			UNUserNotificationCenter.current().add(UNNotificationRequest(identifier: "did_receive_sighting", content: startedNotification, trigger: nil), withCompletionHandler: nil)
-		}
-	}
-
-
+//	func placeManager(_ manager: GMBLPlaceManager!, didEnd visit: GMBLVisit!) {
+//		if (visit.place.identifier == "4C79341D0DC34F7F88B82436DF975DC6")  { // my wallet place
+//			let startedNotification = UNMutableNotificationContent()
+//			startedNotification.title = "Wallet Finder"
+//			startedNotification.body = "You left your wallet behind! Last known location: \(visit.place.attributes)"
+//			startedNotification.categoryIdentifier = "message"
+//			UNUserNotificationCenter.current().add(UNNotificationRequest(identifier: "did_end_visit", content: startedNotification, trigger: nil), withCompletionHandler: nil)
+//
+//		}
+//	}
+//	
+//	func placeManager(_ manager: GMBLPlaceManager!, didBegin visit: GMBLVisit!) {
+//		if (visit.place.identifier == "4C79341D0DC34F7F88B82436DF975DC6")  { // my wallet place
+//			let startedNotification = UNMutableNotificationContent()
+//			startedNotification.title = "Wallet Finder"
+//			startedNotification.body = "Your wallet is nearby! Last known location: \(visit.place.attributes)"
+//			startedNotification.categoryIdentifier = "message"
+//			UNUserNotificationCenter.current().add(UNNotificationRequest(identifier: "did_begin_visit", content: startedNotification, trigger: nil), withCompletionHandler: nil)
+//			
+//		}
+//	}
 }
 
